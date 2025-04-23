0d34f9e34c478cbfba188f685fd6f37516d0ecb7
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-04-23 11:56:55 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
-import { Button } from "./components/ui/button";
+import { Route, Routes } from "react-router-dom";
+import AuthPage from "./pages/auth";
 
 function App() {
   return (
     <div>
-      <Button>Hello</Button>
+      <Routes>
+        <Route path="/auth" element={<AuthPage />} />
+      </Routes>
     </div>
   );
 }
