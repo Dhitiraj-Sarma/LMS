c95f3061a46bdb628e29da7add0e3700352b85a0
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-04-24 17:21:46 +0530

    commit

--- backend/routes/auth-routes.js
+++ backend/routes/auth-routes.js
@@ -1,9 +1,8 @@
 import express from "express";
-import { registerUser } from "../controllers/auth-controller";
+import { registerUser } from "../controllers/auth-controller.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
-
 
 export default router;
