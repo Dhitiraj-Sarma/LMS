da7cc10477892763580aa1a54520adcfce7d5544
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-04-26 16:40:59 +0530

    commit

--- frontend/src/context/auth-context/index.jsx
+++ frontend/src/context/auth-context/index.jsx
@@ -82,6 +82,7 @@ export default function AuthProvider({ children }) {
         setSignUpFormData,
         handleRegisterUser,
         handleLoginUser,
+        auth,
       }}
     >
       {children}
