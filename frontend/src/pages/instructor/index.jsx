2d7527dba2ad51a432fc25081cff4cbae764652a
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-04-27 18:16:32 +0530

    commit

--- frontend/src/pages/instructor/index.jsx
+++ frontend/src/pages/instructor/index.jsx
@@ -30,7 +30,7 @@ function InstructorDashboard() {
 
   function handleLogout() {}
   return (
-    <div className="flex flex-col min-h-screen bg-gray-100">
+    <div className="flex h-full min-h-screen bg-gray-100">
       <aside className="w-64 bg-white shadow-md hidden md:block">
         <div className="p-4">
           <h2 className="text-2xl font-bold mb-4">Instructor View</h2>
