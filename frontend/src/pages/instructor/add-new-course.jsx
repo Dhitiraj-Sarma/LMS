fe71006ed016dea85887afb26c302b8491b074f7
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-04-28 18:13:59 +0530

    new course page

--- frontend/src/pages/instructor/add-new-course.jsx
+++ frontend/src/pages/instructor/add-new-course.jsx
@@ -1,5 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Tabs } from "@/components/ui/tabs";
+
 function AddNewCourse() {
-  return <div>AddNewCourse</div>;
+  return (
+    <div className="container mx-auto p-4">
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-extrabold mb-5">Create a new course</h1>
+        <Button className="text-sm tracking-wider font-bold px-8">
+          SUBMIT
+        </Button>
+      </div>
+      <Card>
+        <CardContent>
+          <div className="container mx-auto p-4">
+            <Tabs defaultValue="curriculum" className="space-y-4"></Tabs>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
 
 export default AddNewCourse;
