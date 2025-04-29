a76c447a70f54f0330b7abded0069f6cb2348d91
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-04-29 13:08:55 +0530

    create instuctor context

---
+++ frontend/src/context/instructor-context/index.jsx
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+export const InstructorContext = createContext(null);
+
+export default function InstructorProvider({ children }) {
+  return <InstructorContext.Provider>{children}</InstructorContext.Provider>;
+}
