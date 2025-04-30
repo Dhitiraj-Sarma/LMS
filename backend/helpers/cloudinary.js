4206b94e1f7b22991c5d83d7db631a3f7615e868
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-04-30 14:59:59 +0530

    setup cloudinary

---
+++ backend/helpers/cloudinary.js
@@ -0,0 +1,19 @@
+import { v2 } from "cloudinary";
+
+v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+const uploadMediaToCloudinary = async (filePath) => {
+  try {
+    const result = await v2.uploader.upload(filePath, {
+      resource_type: "auto",
+    });
+
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error uploading to cloudinary");
+  }
+};
