81b5f65701f0f5f4724e51f2989a0a272caece00
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-05-06 14:29:40 +0530

    creating schema for create new course

---
+++ backend/models/Course.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const lectureSchema = new mongoose.Schema({
+  title: String,
+  videoUrl: String,
+  public_id: String,
+  freePreview: Boolean,
+});
+
+const CourseSchema = new mongoose.Schema({
+  instructorId: String,
+  instructorName: String,
+  date: Date,
+  title: String,
+  category: String,
+  level: String,
+  primaryLanguage: String,
+  subtitle: String,
+  description: String,
+  image: String,
+  welcomeMessage: String,
+  pricing: Number,
+  objectives: String,
+  students: [
+    {
+      studentId: String,
+      studentName: String,
+      studentEmail: String,
+    },
+  ],
+  curriculum: [lectureSchema],
+});
+
+
+const Course = mongoose.model("Course", CourseSchema);
+
+export default Course;
