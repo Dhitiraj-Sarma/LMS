0804cd6d8569e6e8623ad4fc9b02618ce4d938da
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-05-10 13:16:09 +0530

    student homepage setup

--- frontend/src/pages/student/home/index.jsx
+++ frontend/src/pages/student/home/index.jsx
@@ -2,8 +2,15 @@ import { Button } from "@/components/ui/button";
 
 function StudentHomePage() {
   return (
-    <div>
-      <h2>Home Page</h2>
+    <div className="min-h-screen">
+      <section className="flex flex-col lg:flex-row items-center justify-between py-8 px-4 lg:px-8">
+        <div className="lg:w-1/2 lg:pr-12">
+          <h1 className="text-5xl font-bold mb-4">Leaning that gets you</h1>
+          <p className="text-xl">
+            Skills for your present and your future. Get Started with us
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
