14215be531761b92c908db7e391a5e6790d03598
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-05-12 15:59:33 +0530

    setup student context

--- frontend/src/pages/student/home/index.jsx
+++ frontend/src/pages/student/home/index.jsx
@@ -1,3 +1,5 @@
+import { courseCategories } from "@/config";
+import banner from "/banner-img.png";
 import { Button } from "@/components/ui/button";
 
 function StudentHomePage() {
@@ -9,6 +11,29 @@ function StudentHomePage() {
           <p className="text-xl">
             Skills for your present and your future. Get Started with us
           </p>
+        </div>
+        <div className="lg:w-full mb-8 lg:mb-0">
+          <img
+            src={banner}
+            alt="banner"
+            width={600}
+            height={400}
+            className="w-full h-auto rounded-lg shadow-lg"
+          />
+        </div>
+      </section>
+      <section className="py-8 px-4 lg:px-8 bg-gray-100">
+        <h2 className="text-2xl font-bold mb-6">Course Categories</h2>
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
+          {courseCategories?.map((categoryItem) => (
+            <Button
+              className="justify-start"
+              variant="outline"
+              key={categoryItem.id}
+            >
+              {categoryItem.label}
+            </Button>
+          ))}
         </div>
       </section>
     </div>
