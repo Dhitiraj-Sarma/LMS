bc3369f481d30743e54d09d430804b7cd5874c12
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-05-12 16:15:28 +0530

    getAllCourseDetails controller

--- backend/controllers/student-controller.js
+++ backend/controllers/student-controller.js
@@ -27,6 +27,21 @@ export const getAllCourses = async (req, res) => {
 
 export const getAllCourseDetails = async (req, res) => {
   try {
+    const { id } = req.params;
+    const courseDetails = await Course.findById(id);
+
+    if (!courseDetails) {
+      return res.status(404).json({
+        success: false,
+        message: "No Course details Found",
+        data: [],
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: courseDetails,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({
