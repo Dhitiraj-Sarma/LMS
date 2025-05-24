12bb5aae9d367a749bd4464d5dd32f0d64f0b34b
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-05-24 21:47:15 +0530

    studnet context

--- frontend/src/context/student-context/index.jsx
+++ frontend/src/context/student-context/index.jsx
@@ -5,9 +5,22 @@ export const StudentContext = createContext(null);
 export default function StudentProvider({ children }) {
   const [studentCoursesList, setStudentCoursesList] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [studentCourseDetails, setStudentCourseDetails] = useState(null);
+  const [currentStudentCourseDetails, setCurrentStudentCourseDetails] =
+    useState(null);
+
   return (
     <StudentContext.Provider
-      value={{ studentCoursesList, setStudentCoursesList, loading, setLoading }}
+      value={{
+        studentCoursesList,
+        setStudentCoursesList,
+        loading,
+        setLoading,
+        studentCourseDetails,
+        setStudentCourseDetails,
+        currentStudentCourseDetails,
+        setCurrentStudentCourseDetails,
+      }}
     >
       {children}
     </StudentContext.Provider>
